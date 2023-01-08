01bc00bbf01a46cf83000a7cbb3cd809fad53524
@babinm1h <babinmih@mail.ru>
2023-01-08 17:32:41 +0300

    env

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -9,21 +9,50 @@ import { User } from 'src/typeorm/entities/user.entity';
 
 export const getTypeOrmConfig = async (
   cfg: ConfigService,
-): Promise<TypeOrmModuleOptions> => ({
-  type: 'postgres',
-  host: 'localhost',
-  autoLoadEntities: true,
-  synchronize: true,
-  port: cfg.get('DB_PORT'),
-  password: cfg.get('DB_PASSWORD'),
-  username: cfg.get('DB_USERNAME'),
-  database: cfg.get('DB_NAME'),
-  entities: [
-    User,
-    Dialog,
-    Message,
-    GroupDialog,
-    MessageAttachment,
-    FriendRequest,
-  ],
-});
+): Promise<TypeOrmModuleOptions> => {
+  let dbCfg: TypeOrmModuleOptions;
+
+  if (process.env.NODE_ENV === 'prod') {
+    dbCfg = {
+      type: 'postgres',
+      host: 'dpg-cetd0t1gp3jmgl0ho4cg-a',
+      url: 'postgres://user:cxzfEdOnOhO8KWnnMWaqLqBoG9noDJIQ@dpg-cetd0t1gp3jmgl0ho4cg-a.frankfurt-postgres.render.com/chat_mcxl',
+      autoLoadEntities: true,
+      synchronize: true,
+      port: cfg.get('DB_PORT'),
+      password: `cxzfEdOnOhO8KWnnMWaqLqBoG9noDJIQ`,
+      username: 'user',
+      database: 'chat_mcxl',
+      ssl: true,
+      entities: [
+        User,
+        Dialog,
+        Message,
+        GroupDialog,
+        MessageAttachment,
+        FriendRequest,
+      ],
+    };
+  } else {
+    dbCfg = {
+      type: 'postgres',
+      host: 'localhost',
+      autoLoadEntities: true,
+      synchronize: true,
+      port: cfg.get('DB_PORT'),
+      password: cfg.get('DB_PASSWORD'),
+      username: cfg.get('DB_USERNAME'),
+      database: cfg.get('DB_NAME'),
+      entities: [
+        User,
+        Dialog,
+        Message,
+        GroupDialog,
+        MessageAttachment,
+        FriendRequest,
+      ],
+    };
+  }
+
+  return dbCfg;
+};
